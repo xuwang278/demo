230f5b47c3350443614149e4760666a0b2115c7b
Xu Wang <xuwang278@fb.com>
2021-08-24 10:23:57 -0700

    added cat functionality

--- animal.py
+++ animal.py
@@ -3,8 +3,14 @@ import sys
 def default():
 	print('Hello')
 
+def cat():
+	print('Meow')
+
 def main():
-	default()
+	if sys.argv[1] == 'cat':
+		cat()
+	else:
+		default()
 
 if __name__ == '__main__':
 	main()
