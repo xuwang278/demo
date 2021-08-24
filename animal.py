e1ccbeb55139dd53dea1eb1cce89cbbc8e69fd55
Xu Wang <xuwang278@fb.com>
2021-08-24 10:36:22 -0700

    added cow functionality

--- animal.py
+++ animal.py
@@ -3,8 +3,14 @@ import sys
 def default():
 	print('Hello')
 
+def cow():
+	print('cow')
+
 def main():
-	default()
+	if sys.argv[1] == 'cow':
+		cow()
+	else:
+		default()
 
 if __name__ == '__main__':
 	main()
